ed7fb83075e33ef94cabf69f216d2ef76e2fb1bf
Julian Alexis Huchin Garcia <17470084@campeche.tecnm.mx>
2021-07-04 17:41:50 -0500

    Versión inicial del proyecto

---
+++ BooksApp.Domain/Class1.cs
@@ -0,0 +1,32 @@
+﻿using BooksApp.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace BooksApp.Domain
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
+    public class Publisher 
+    { 
+        public int Id { get; set; } 
+        public string PublisherName { get; set; } 
+    }
+
+public class Book
+{
+    public Book() { Authors = new List<Author>(); }
+    public int Id { get; set; }
+    public string Isbn { get; set; }
+    public string Title { get; set; }
+    public int EditionNumber { get; set; }
+    public string Copyright { get; set; }
+    public List<Author> Authors { get; set; }
+    public Publisher PublishingHouse { get; set; }
+    public void AddAuthor(Author author) { if (author != null) { Authors.Add(author); } }
+}
